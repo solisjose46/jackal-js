c96c75f954f5c8a42ed8b793be540a77b367b1b8
Jose Solis <solisjose46@Joses-iMac.local>
2021-04-12 16:54:05 -0700

    added update and fyi info, and game update

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+//lots of redundant code, will consolidate later
 var game = document.getElementById('game');
 var view = document.getElementById('view');
 var stage = document.getElementById('stage');
+var character = document.getElementById('character');
+var tanklib = ['blue-n.png', 'blue-ne.png', 'blue-e.png', 'blue-se.png', 'blue-s.png', 'blue-sw.png', 'blue-w.png', 'blue-nw.png'];
 var BOUND_TOP = -105; //in px
 var BOUND_BOTTOM = -888;
 
@@ -16,6 +19,9 @@ function movingUp(){
         str = str+=5;
         stage.style.marginTop = str + 'px';
     }
+    else{
+        console.log("view at end");
+    }
 
 }
 
@@ -29,8 +35,44 @@ function movingDown(){
         str = str-=5;
         stage.style.marginTop = str + 'px';
     }
+    else{
+        console.log("view at end");
+    }
 }
 
+function movingLeft(){
+    var img = character.src;
+    
+    for(var i=0; i < tanklib.length; i++){
+        var searching = img.search(tanklib[i]);
+        if(searching != -1){
+            if(i==0){
+                img = tanklib[7];
+            }
+            else{
+                img = tanklib[ i - 1];
+            }
+            break;
+        }
+    }
+    character.src = './tanks/' + img;
+}
+function movingRight(){
+    var img = character.src;
+    for(var i=0; i < tanklib.length; i++){
+        var searching = img.search(tanklib[i]);
+        if(searching != -1){
+            if(i==7){
+                img = tanklib[0];
+            }
+            else{
+                img = tanklib[ i + 1];
+            }
+            break;
+        }
+    }
+    character.src = './tanks/' + img;
+}
 function keyDownHandler(event)
 {
 	var keyPressed = String.fromCharCode(event.keyCode);
@@ -40,13 +82,13 @@ function keyDownHandler(event)
             movingUp();
             break;
         case 'A':
-            console.log(keyPressed);
+            movingLeft();
             break;
         case 'S':
             movingDown();
             break;
         case 'D':
-            console.log(keyPressed);
+            movingRight();
             break;        
     }
 }
