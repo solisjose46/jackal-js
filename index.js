5e0e86a1296765433fda2cd71592dbaa311ae830
Jose Salazar <solisjose466@gmail.com>
2021-05-20 15:53:25 -0700

    implemented classes to reduce redundant code

---
+++ index.js
@@ -0,0 +1,51 @@
+//constructor(html, image, role, direction, speedLimit, library)
+var temp = document.createElement('DIV');
+temp.className = 'mapObjects';
+var tempImg = document.createElement('IMG');
+tempImg.src = 'tanks/blue-n.png'
+temp.appendChild(tempImg);
+temp.style.height = '30px';
+temp.style.width = '30px';
+temp.style.top = '2500px';
+temp.style.left = '150px';
+game.appendChild(temp);
+var role = 'character';
+var direction = 'north';
+var speedLimit = 10;
+var player = new Player(temp, tempImg, role, direction, speedLimit, tankLibrary);
+
+
+//key handler
+var map = {};
+onkeydown = onkeyup = function(e){
+    e = e || event;
+    map[e.key] = e.type == 'keydown';
+    e.preventDefault();
+    if(map['w'] && map['d']){
+        player.moveObject('north-east');
+    }
+    else if(map['w'] && map['a']){
+        player.moveObject('north-west');
+    }
+    else if(map['s'] && map['d']){
+        player.moveObject('south-east');
+    }
+    else if(map['s'] && map['a']){
+        player.moveObject('south-west');
+    }
+    else if(map['w']){
+        player.moveObject('north');
+    }
+    else if(map['d']){
+        player.moveObject('east');
+    }
+    else if(map['s']){
+        player.moveObject('south');
+    }
+    else if(map['a']){
+        player.moveObject('west');
+    }
+    if(map[' ']){
+        player.shootRocket();
+    }
+}
--- objects.js
+++ objects.js
@@ -1,19 +1,20 @@
 var game = document.getElementById('game-container');
-var mapObjects = Array.from(document.getElementsByClassName('mapObjects'));
-class Object{
-    constructor(width, height, left, top, imageSrc, role, index){
-        this.width = width;
-        this.height = height;
-        this.left = left;
-        this.top = top;
-        this.styleWidth = width + 'px';
-        this.styleHeight = height + 'px';
-        this.styleLeft = left + 'px';
-        this.styleTop = top + 'px';
-        this.image = imageSrc; //change at inherited classes
-        this.status = false; 
-        this.role = role; //does not change
-        this.index = index; //index in class of map arrays
+var mapObjects = []; //player, removeable, none, turret, nonremovable 
+
+class GameObject{
+    constructor(html, image, role, direction){
+        this.html = html;
+        this.styleWidth = window.getComputedStyle(html).width;
+        this.styleHeight = window.getComputedStyle(html).height;
+        this.styleLeft = window.getComputedStyle(html).left;
+        this.styleTop = window.getComputedStyle(html).top;
+        this.width = parseInt(this.styleWidth.replace('px', ''));
+        this.height = parseInt(this.styleHeight.replace('px', ''));
+        this.left = parseInt(this.styleLeft.replace('px', ''));
+        this.top = parseInt(this.styleTop.replace('px', ''));
+        this.image = image;
+        this.role = role;
+        this.direction = direction;
     }
     //getters
     get getStyles(){
@@ -34,45 +35,236 @@ class Object{
         };
         return toReturn;
     }
-    get getImage(){
-        return this.image;
-    }
-    get getStatus(){
-        return this.status;
-    }
-    get getRole(){
-        return this.role;
-    }
-    //setter
-    set setLeft(left){
-        this.left = left;
-        this.styleLeft = this.left + 'px';
-        mapObjects[this.index].style.left = this.styleLeft;
-    }
-    set setTop(top){
-        this.top = top;
-        this.styleTop = this.top + 'px';
-        mapObjects[this.index].style.top = this.styleTop;
-    }
-}
-
-class Shooter extends Object{
-    constructor(direction, width, height, left, top, imageSrc, role, index){
-        super(width, height, left, top, imageSrc, role, index);
-        this.direction = direction;
-    }
-    //getter
     get getDirection(){
         return this.direction;
     }
+    get getRole(){
+        return this.rolel
+    }
     //setter
-    set setDirection(direction){
+    setLeft(left){
+        this.left = left;
+        this.styleLeft = this.left + 'px';
+        this.html.style.left = this.styleLeft;
+    }
+    setTop(top){
+        this.top = top;
+        this.styleTop = this.top + 'px';
+        this.html.style.top = this.styleTop;
+    }
+    changeImage(imageSrc){
+        this.image.src = imageSrc;
+    }
+    setDirection(direction){
         this.direction = direction;
     }
-    Shoot(){
-        //
+    inBounds(lng, lat){
+        //lng = this.left; lat = this.top;
+        //credit to: https://rosettacode.org/wiki/Ray-casting_algorithm
+        var count = 0;
+        for (var b = 0; b < bounds.length; b++) {
+            var vertex1 = bounds[b];
+            var vertex2 = bounds[(b + 1) % bounds.length];
+            if (west(vertex1, vertex2, lng, lat))
+                ++count;
+        }
+        return count % 2;
+     
+        /**
+         * @return {boolean} true if (x,y) is west of the line segment connecting A and B
+         */
+        function west(A, B, x, y) {
+            if (A.y <= B.y) {
+                if (y <= A.y || y > B.y ||
+                    x >= A.x && x >= B.x) {
+                    return false;
+                } else if (x < A.x && x < B.x) {
+                    return true;
+                } else {
+                    return (y - A.y) / (x - A.x) > (B.y - A.y) / (B.x - A.x);
+                }
+            } else {
+                return west(B, A, x, y);
+            }
+        }
     }
-}
-// class Projectile extends Object{
-//     constructor()
-// }
+};
+
+class Projectile extends GameObject{
+    constructor(html, image, role, direction, speedLimit){
+        super(html, image, role, direction);
+        this.speedLimit = speedLimit;
+    }
+    moveX(){
+        var newLeft;
+        var speedLimit = this.speedLimit;
+        if(this.direction.includes('west')){
+            speedLimit = this.speedLimit * -1;
+        }
+        var newLeft = this.left + speedLimit;
+        return newLeft;
+    }
+    moveY(){
+        var newTop;
+        var speedLimit = this.speedLimit;
+        if(this.direction.includes('north')){
+            speedLimit = this.speedLimit * -1;
+        }
+        newTop = this.top + speedLimit;
+        return newTop;
+    }
+    move(direction){
+        this.setDirection(direction);
+        var lng; var lat;
+        if(this.direction == 'north' || this.direction == 'south'){
+            lng = this.left;
+            lat = this.moveY();
+        }
+        else if(this.direction == 'east' || this.direction == 'west'){
+            lng = this.moveX();
+            lat = this.top;
+        }
+        else{
+            lng = this.moveX();
+            lat = this.moveY();
+            
+        }
+        if(this.inBounds(lng, lat) == 1){
+            this.setLeft(lng);
+            this.setTop(lat);
+            return true;
+        }
+        return false;
+    }
+    projectileCollision(obstacles){
+        
+        var toReturn = {
+            impact: true,
+            object: 'bounds',
+            index: null
+        };
+ 
+        if(this.inBounds(this.left, this.top) == 1){
+            var obstacle;
+            for(var i = 0; i < obstacles.length; i++){
+                obstacle = obstacles[i];
+                if(this.collision(this, obstacle)){
+                    toReturn.object = obstacle.getRole;
+                    toReturn.index = i;
+                }
+            }
+        }
+        return toReturn;
+    }
+    collision(object1, object2){
+        var rectangleOne = object1.getNums;
+        var rectangleTwo = object2.getNums;
+        if (rectangleOne.left < rectangleTwo.right && rectangleOne.right > rectangleTwo.left && rectangleOne.top < rectangleTwo.bottom && rectangleOne.bottom > rectangleTwo.top) {
+            return true; //collision detected
+        }
+        return false;        
+    }
+};
+
+class Shooter extends Projectile{
+    constructor(html, image, role, direction, speedLimit, library){
+        super(html, image, role, direction, speedLimit);
+        this.library = library;
+    }
+    rotate(direction){
+        if(this.direction != direction){
+            var position = this.library.indexOf(this.library.find(({direction}) => direction === this.direction));
+            var right = false;
+            var currentPosition = position;
+            var minTurns = 4;
+            for(var i=0; i < minTurns; i++){
+                if(currentPosition != 7){
+                    currentPosition++;
+                }
+                else{
+                    currentPosition = 0;
+                }
+                if(this.library[currentPosition].direction == direction){
+                    right = true;
+                    break;
+                }
+            }
+            if(right){
+                if(position != 7){
+                    position++;
+                }
+                else{
+                    position = 0;
+                }
+            }
+            else{
+                if(position != 0){
+                    position--;
+                }
+                else{
+                    position = 7;
+                }
+            }
+            this.direction = this.library[position].direction;
+            this.changeImage(this.library[position].img);
+            return false;
+        }
+        return true;
+    }
+    moveObject(direction){
+        if(this.rotate(direction)){
+            this.move(this.direction);
+            return true;
+        }
+        return false;
+    }
+    shoot(rocketImage, obstacles){
+        var rocket = new Rocket(rocketImage, this.left, this.top, this.direction, this.role);
+        rocket.flight(obstacles);
+    }
+};
+
+class Rocket extends Projectile{
+    //constructor(html, image, role, direction, speedLimit)
+    constructor(rocketImage, left, top, direction, shooter){
+        this.shooter = shooter;
+        var temp = document.createElement('DIV');
+        temp.className = 'mapObjects';
+        var tempImg = document.createElement('IMG');
+        tempImg.src = rocketImage;
+        temp.appendChild(tempImg);
+        temp.style.height = '20px';
+        temp.style.width = '20px';
+        temp.style.left = left + 'px';
+        temp.style.top = top + 'px';
+        game.appendChild(temp);
+        var speedLimit = 15;
+        super(temp, tempImg, 'rocket', direction, speedLimit);
+    }
+    flight(obstalces){
+        
+    }
+};
+
+class Player extends Shooter{
+    constructor(html, image, role, direction, speedLimit, library){
+        super(html, image, role, direction, speedLimit, library);
+    }
+    shootRocket(){
+        //left off here!!!
+        var rocketImage = rocketLibrary.find(({direction}) => direction === this.direction).img;
+        var obstacles = [];
+        this.shoot(rocketImage, obstacles);
+    }
+};
+
+// class Turret extends Shooter{
+//     constructor(html, image, role, direction, speedLimit, library){
+//         super(html, image, role, direction, speedLimit, library);
+//     }
+//     shootRocket(){
+//         var rocketImage = 'assets/weapons/projectile.png';
+//         var obstacles = [];
+//         this.shoot(rocketImage, obstacles);
+//     }
+// };
